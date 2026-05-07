bc786a71584fbf91690462a5b27a28096bf0e519
Hudson <hudson.b.araujo@gmail.com>
2026-05-07 14:54:22 -0300

    feat: implementando model de login do usuário

---
+++ LojaVirtual/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtual/Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LojaVirtual.Models
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = string.Empty;
+    }
+}
