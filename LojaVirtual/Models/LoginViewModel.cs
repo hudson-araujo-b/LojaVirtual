2e9436e36ad65f88de4f9fa5f46554573097cbbe
Hudson <hudson.b.araujo@gmail.com>
2026-05-07 16:52:52 -0300

    feat: adicionando validação de dados na model de login

--- LojaVirtual/Models/LoginViewModel.cs
+++ LojaVirtual/Models/LoginViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace LojaVirtual.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaVirtual.Models
 {
     public class LoginViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
         public string Nome { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A senha é obrigatória.")]
         public string Senha { get; set; } = string.Empty;
         public string Nivel { get; set; } = string.Empty;
     }
